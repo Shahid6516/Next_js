f2c291018f23201ca130861aaf034cfcc38aeb08
Mohammad Shahid <www.shahid6516@gmail.com>
2026-03-25 07:42:22 +0530

    feat: add Nav component and integrate it into RootLayout

---
+++ app/nav/page.tsx
@@ -0,0 +1,7 @@
+export default function Nav(){
+    return(
+        <>
+        <h1 className="bg-red-700 border-2 border-dotted border-amber-400">This is the Nav component</h1>
+        </>
+    )
+}
